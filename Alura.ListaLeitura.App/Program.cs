b05aebcff9df8211c5bce595fbeb80acfac79c9a
Rene Pereira Milare <milarerp@gmail.com>
2020-10-20 11:13:22 -0300

    Constrído request pipeline

--- Alura.ListaLeitura.App/Program.cs
+++ Alura.ListaLeitura.App/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Alura.ListaLeitura.App.Negocio;
 using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Hosting;
 using System;
 
 namespace Alura.ListaLeitura.App
@@ -10,9 +11,16 @@ namespace Alura.ListaLeitura.App
         {
             var _repo = new LivroRepositorioCSV();
 
-            ImprimeLista(_repo.ParaLer);
-            ImprimeLista(_repo.Lendo);
-            ImprimeLista(_repo.Lidos);
+            /* A classe "WebHostBuilder" é responsável por construir um hospedeiro web, o método "Build" cria uma implementação da interface "IWebHost", precisamos também dizer qual é a implementação do modelo HTTP que estamos utilizando, fazemos isso com o método "UseKestrel", é preciso também uma classe que inicializa esse host utilizamos o método "UseStartup" passando a classe por ele. */
+            IWebHost host = new WebHostBuilder()
+                .UseKestrel()
+                .UseStartup<Startup>()
+                .Build();
+            host.Run();
+
+            //ImprimeLista(_repo.ParaLer);
+            //ImprimeLista(_repo.Lendo);
+            //ImprimeLista(_repo.Lidos);
         }
 
         static void ImprimeLista(ListaDeLeitura lista)
---
+++ Alura.ListaLeitura.App/Startup.cs
@@ -0,0 +1,25 @@
+﻿using Alura.ListaLeitura.App.Repositorio;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Http;
+using System.Threading.Tasks;
+
+namespace Alura.ListaLeitura.App
+{
+    /* A classe "Startup" precisa implementar o método "Configure" para startar o servidor */
+    public class Startup
+    {
+        /* Configurando a sequêncai de resposta (Request Pipeline), e a configuração desse fluxo requisição-resposta, também fica em um tipo do Asp.NET Core que é o "IApplicationBuilder", que vai construir todo o pipeline para a aplicação.*/
+        public void Configure(IApplicationBuilder app)
+        {
+            app.Run(LivrosParaLer);
+        }
+
+        /* Toda informação que estiver encapsulada em uma requisição específica, fica armazenada em objetos do tipo "HttpContext". Precisamos retornar um objeto do tipo Task para que o "IApplicationBuilder" consiga construir o pipeline. Objetos do tipo Task são abordados no curso de paralelismo. */
+        public Task LivrosParaLer(HttpContext context)
+        {
+            var _repo = new LivroRepositorioCSV();
+            /* Quando a requisição chegar vamos responder escrevendo na tela */
+            return context.Response.WriteAsync(_repo.ParaLer.ToString());
+        }
+    }
+}
